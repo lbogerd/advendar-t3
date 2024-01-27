577bf9c3012912d55a620a9730d16e96d69af05b
Lucas Bogerd <lucas@youngones.works>
2024-01-27 11:13:58 +0100

    Add Inter font to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,26 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { Inter } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+
+const inter = Inter({
+  subsets: ["latin", "latin-ext"],
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <div className={inter.className}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </div>
   );
 };
 
