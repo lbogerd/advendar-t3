8fae7c29ea27feee05e0dd6bd611f75f6522616b
Lucas Bogerd <lucas@youngones.works>
2024-02-04 14:47:22 +0100

    Finish /new page v1

---
+++ src/components/ui/text-area.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+import { cn } from "~/lib/utils";
+
+export type TextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background transition placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-yellow-400 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+TextArea.displayName = "Textarea";
+
+export { TextArea };
