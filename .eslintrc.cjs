decd9508564ecfef305d91d5d29da649a580acab
Lucas Bogerd <lucas@youngones.works>
2024-02-17 00:33:35 +0100

    Add ignore patterns to .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  ignorePatterns: ["**.js", "node_modules", ".next", "out"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
