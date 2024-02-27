3c5297ce5aec01cfd4f0577f2a2036d0ce72be09
lbogerd <l.bogerd@protonmail.com>
2024-02-27 14:17:48 +0100

    Add @tailwindcss/container-queries plugin and update body class in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en" className="h-full">
       <Head />
-      <body className="h-full bg-yellow-100/15">
+      <body className="h-full overscroll-none bg-yellow-100/15">
         <Main />
         <NextScript />
       </body>
