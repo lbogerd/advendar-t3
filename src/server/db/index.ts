e8c2adf41c4f0a3d35cb924f11e7e4eb4d6c11fc
lbogerd <l.bogerd@protonmail.com>
2024-02-27 13:44:13 +0100

    Fix database init to prevent "too many clients" error in dev

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,26 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 import { env } from "~/env.js";
 import * as schema from "./schema";
 
-export const db = drizzle(postgres(env.DATABASE_URL), { schema });
+// Prevents "too many clients" error in development
+declare global {
+  // Only var (not const/let) works here
+  // eslint-disable-next-line no-var
+  var db: PostgresJsDatabase<typeof schema> | undefined;
+}
+
+let db: PostgresJsDatabase<typeof schema>;
+
+if (process.env.NODE_ENV === "production") {
+  db = drizzle(postgres(env.DATABASE_URL), { schema });
+} else {
+  if (!global.db) {
+    global.db = drizzle(postgres(env.DATABASE_URL), { schema });
+  }
+
+  db = global.db;
+}
+
+export { db };
