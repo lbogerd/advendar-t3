6f06d8f867082a1f5d9d6f1e9e54ed092edb4d65
Lucas Bogerd <lucas@youngones.works>
2024-03-03 01:51:21 +0100

    Redirect to dashboard if authenticated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const session = useSession();
+  const router = useRouter();
+
+  if (session.status === "authenticated") {
+    router.push("dashboard");
+  }
+
   return (
     <>
       <Head>
